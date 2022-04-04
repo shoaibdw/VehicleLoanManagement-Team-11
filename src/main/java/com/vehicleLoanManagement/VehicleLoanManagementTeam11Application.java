9c6941f6e7fd4ffaf869a08b35623927b75e7e21
Shoaib Khan <shoaib.khan@atcs.com>
2022-04-04 11:50:40 +0530

    fist commit

---
+++ src/main/java/com/vehicleLoanManagement/VehicleLoanManagementTeam11Application.java
@@ -0,0 +1,13 @@
+package com.vehicleLoanManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleLoanManagementTeam11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleLoanManagementTeam11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vehicleLoanManagement/VehicleLoanManagementTeam11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicleLoanManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleLoanManagementTeam11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
